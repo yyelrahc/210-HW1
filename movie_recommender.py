3a26de17cac34c1d0e7034ab45be573f486ca28e
yyelrahc <charleyyang602@gmail.com>
2026-03-05 07:33:08 -0500

    initial commit
    hi

---
+++ movie_recommender.py
@@ -0,0 +1,17 @@
+'''
+# Coding Specification
+#     Build an executable python program, movie_recommender.py which can be run like this:
+#        > python movie_recommender.py
+#     Your program should have a command line interface (CLI) with menus to load input data 
+#     files and test each function so you can run the program and test for yourself
+#     Add docstring documentation to each function
+#     Ensure that your code will work with Python 3.12 
+'''
+import sys
+
+def main():
+    print("hi")
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ test_movie_recommender.py
@@ -0,0 +1,17 @@
+'''
+# Testing Specification
+#     Have the chatbot generate a python program called test_movie_recommender.py to do 
+#     automatic testing. The tester code should be runnable like this:
+#        > python test_movie_recommender.py
+#     It should load input files, call each function, and print the result. This is quicker 
+#     than manually running the original program and individually testing each menu option.
+#     Compare printed results against expected values so you know the code is correct. 
+'''
+import sys
+
+def main():
+    print("hi")
+    pass
+
+if __name__ == "__main__":
+    main()
